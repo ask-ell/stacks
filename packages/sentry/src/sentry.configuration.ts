99e66533bc5ee28d2d9bc7eb2c5aa3d70327742f
Yannick Debree <askellcompany@gmail.com>
2026-04-01 14:36:17 +1300

    Sentry package created

---
+++ packages/sentry/src/sentry.configuration.ts
@@ -0,0 +1,41 @@
+import type { ILogger, MaybeUndefined } from "@ask-ell/core";
+import type { Client, Options } from "@sentry/core"
+
+
+type SentryClientFactory = (options: Options) => MaybeUndefined<Client>;
+
+export type SentryConfigurationProps = {
+    publicKey: string;
+    host: string;
+    projectId: string;
+    environment?: MaybeUndefined<string>;
+    logger: ILogger;
+    init: SentryClientFactory;
+};
+
+export class SentryConfiguration {
+    private inited = false;
+    private logger: ILogger;
+
+    constructor(private props: SentryConfigurationProps) {
+        this.logger = this.props.logger;
+    }
+
+    init(): void {
+        if(this.inited){
+            return this.logger.info("Sentry already inited. Step skipped");
+        }
+
+        const { publicKey, host, projectId, environment, init } = this.props;
+        const dsn: string = `${publicKey}@${host}/${projectId}`;
+
+        init({
+            dsn,
+            sendDefaultPii: true,
+            environment: environment ?? "production"
+        });
+
+        this.logger.info(`Sentry inited for environment "${environment}"`);
+        this.inited = true;
+    }
+}
