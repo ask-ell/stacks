db6a97e0ec0036fb5eb29a1ac6a19fd1a3ce135e
Yannick Debree <askellcompany@gmail.com>
2026-04-01 15:03:23 +1300

    Nest sentry configuration class created

---
+++ packages/nest/src/nest.sentry.configuration.ts
@@ -0,0 +1,17 @@
+import { init } from "@sentry/nestjs";
+import { SentryConfiguration, SentryConfigurationProps } from "@ask-ell/sentry";
+import { NestLogger } from "@ask-ell/nest";
+
+
+type NestSentryConfigurationProps = Omit<SentryConfigurationProps, "environment" | "logger" | "init">;
+
+export class NestSentryConfiguration extends SentryConfiguration {
+    constructor(props: NestSentryConfigurationProps) {
+        super({
+            ...props,
+            init,
+            logger: NestLogger.fromClass(NestSentryConfiguration),
+            environment: process.env["NODE_ENV"],
+        })
+    }
+}
