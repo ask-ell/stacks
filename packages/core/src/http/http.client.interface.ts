b42edbf34e3320a18f9f4eb0f0446a25c4f29377
Yannick Debree <askellcompany@gmail.com>
2026-07-18 14:45:47 +1200

    core > http module refactored

---
+++ packages/core/src/http/http.client.interface.ts
@@ -0,0 +1,11 @@
+import type { IResult } from '../result'
+import type { DeleteMethodParams, GetMethodParams, PatchMethodParams, PostMethodParams, PutMethodParams } from './types'
+
+
+export interface IHttpClient {
+  get<ResponseData>({ url, headers }: GetMethodParams): Promise<IResult<ResponseData>>;
+  post<Body, ResponseData>({ url, headers, body }: PostMethodParams<Body>): Promise<IResult<ResponseData>>;
+  patch<Body, ResponseData>({ url, headers, body }: PatchMethodParams<Body>): Promise<IResult<ResponseData>>;
+  put<Body, ResponseData>({ url, headers, body }: PutMethodParams<Body>): Promise<IResult<ResponseData>>;
+  delete<Body, ResponseData>({ url, headers, body }: DeleteMethodParams<Body>): Promise<IResult<ResponseData>>;
+}
