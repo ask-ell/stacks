a78d61d9f35f56f866c413cd8938809c66a2bc11
Yannick Debree <askellcompany@gmail.com>
2026-07-18 14:20:05 +1200

    Core exports refactored

--- packages/core/src/event-listeners/event.listener.ts
+++ packages/core/src/event-listeners/event.listener.ts
@@ -1,11 +1,12 @@
 import { type IEventListener, type EventListenerCallback } from './event.listener.interface'
 
+
 export class EventListener<EventData> implements IEventListener<EventData> {
   private readonly callbacks: Array<EventListenerCallback<EventData>> = []
 
-  handle(projectDeleted: EventData): void {
+  handle(eventData: EventData): void {
     this.callbacks.forEach((callback: EventListenerCallback<EventData>): void => {
-      callback(projectDeleted)
+      callback(eventData)
     })
   }
 
