c9e58a27680cd61d583ec564f787df8e908d5972
Yannick Debree <askellcompany@gmail.com>
2026-07-24 21:10:11 +1200

    K8S package refactored

---
+++ packages/k8s/src/middlewares/merge-patch.middleware.ts
@@ -0,0 +1,10 @@
+import { Observable, ObservableMiddleware, RequestContext, ResponseContext } from "@kubernetes/client-node";
+
+
+export const MERGE_PATCH_MIDDLEWARE: ObservableMiddleware = {
+    pre: (context: RequestContext): Observable<RequestContext> => {
+        context.setHeaderParam('Content-Type', 'application/merge-patch+json');
+        return new Observable(Promise.resolve(context));
+    },
+    post: (context: ResponseContext):  Observable<ResponseContext> => new Observable(Promise.resolve(context))
+};
--- packages/k8s/src/utils.ts
+++ packages/k8s/src/utils.ts
@@ -1,6 +1,18 @@
+import { MaybeUndefined } from "@ask-ell/core";
+import { V1CustomResourceDefinitionVersion } from "@kubernetes/client-node";
+
 import { ComponentsFetchingResponse } from "./types";
 
+
 export async function getItems<T>(itemsFactory: Promise<ComponentsFetchingResponse<T>>): Promise<T[]> {
-    const { body: { items } } = await itemsFactory;
+    const { items } = await itemsFactory;
     return items;
+}
+
+export function getVersionName(version: V1CustomResourceDefinitionVersion): string {
+    const versionName: MaybeUndefined<string> = version?.name;
+    if(!versionName){
+        throw new Error('No version specified');
+    }
+    return versionName;
 }
