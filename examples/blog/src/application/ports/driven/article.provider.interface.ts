75f52e2e81563d1b95cb3652b308d9fbbc199e75
Yannick Debree <askellcompany@gmail.com>
2026-08-05 17:13:41 +1200

    Blog package lint fixed

--- examples/blog/src/application/ports/driven/article.provider.interface.ts
+++ examples/blog/src/application/ports/driven/article.provider.interface.ts
@@ -3,4 +3,4 @@ import type { IAggregateRootProvider } from '@ask-ell/hexa'
 import type { ArticleState } from '../../domain'
 
 
-export type IArticleProvider = IAggregateRootProvider<ArticleState>
+export interface IArticleProvider extends IAggregateRootProvider<ArticleState> {}
